bf6205129e7c6b1a35599a7456de99482d079b7d
xiaojunzaizai <49795431+xiaojunzaizai@users.noreply.github.com>
2021-02-05 16:46:32 -0500

    统计字母频率

---
+++ 统计字母.py
@@ -0,0 +1,17 @@
+#单线程 统计字母
+S = 'Orubkiusvazkxyioktikgtjvxobgie'
+S = S.lower()
+d = {}
+for i in S:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] = 1
+
+items = d.items()
+backS = [[v[1],v[0]] for v in items]
+backS.sort(reverse= True)
+
+for i in backS:
+    print(i[1] + ':', end ='  ')
+    print(i[0])
