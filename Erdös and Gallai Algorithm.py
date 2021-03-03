4790960d71466bbfc3543a780e8a6d333e4afec8
xiaojunzaizai <49795431+xiaojunzaizai@users.noreply.github.com>
2021-03-03 03:41:25 -0500

    Erdös and Gallai Algorithm

---
+++ Erdös and Gallai Algorithm.py
@@ -0,0 +1,33 @@
+def algorithm(d):
+    l = len(d)
+    s1 = []
+    s2 = []
+    sum1 = 0
+    for i in range(l):
+        sum1 = sum1+ d[i]
+        s1.append(sum1)
+        sum2 = i*(i+1)
+        for j in range(i+1,l):
+            if i+1<=d[j]:
+                sum2 = sum2+i+1
+                print(i+1,end = ' + ')
+            else:
+                sum2 = sum2+d[j]
+                print(d[j],end = ' + ')
+        print()
+        s2.append(sum2)
+    result = True
+    for k in range(len(s1)):
+        if s1[k] > s2[k]:
+            result = False
+            break
+    return s1,s2,result
+
+
+
+nodes = [9,9,9,7,7,7,6,4,4,4]
+s1,s2,result = algorithm(nodes)
+
+print(s1)
+print(s2)
+print(result)
