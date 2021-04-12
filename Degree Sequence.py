a1830a14bc1ebaa20ba0361b3c89fd62cf423f57
xiaojunzaizai <49795431+xiaojunzaizai@users.noreply.github.com>
2021-04-12 00:10:49 -0400

    test

--- Degree Sequence.py
+++ Degree Sequence.py
@@ -10,6 +10,14 @@ def I_degree_sequence(d, i,j):
             m = sum
     return m
 
+def new_I_degree_sequence(d,i,j):
+    i = i-1
+    j = j-1
+    sum = 0
+    for l in range(i,j+1):
+        sum = sum + abs(d[i] - d[l])
+
+    return sum
 
 def DA(d,i,k,I):
     if i <2*k:
@@ -17,7 +25,7 @@ def DA(d,i,k,I):
     else:
         min = 10000000000000
         for t in range(k,i-k+1):
-            sum = DA(d,t,k,I) + I_degree_sequence(d,t+1,i)
+            sum = DA(d,t,k,I) + new_I_degree_sequence(d,t+1,i)
             if sum < min:
                 min = sum
         if min <= I[i-1]:
@@ -30,7 +38,7 @@ nodes = [10,9 ,9, 8, 7, 7, 7, 7, 6, 6]
 
 I = []
 for i in range(1,len(nodes)+1):
-    I.append(I_degree_sequence(nodes,1,i))
+    I.append(new_I_degree_sequence(nodes,1,i))
 da = []
 for i in range(1,len(nodes)+1):
     da.append(DA(nodes,i,3,I))    
